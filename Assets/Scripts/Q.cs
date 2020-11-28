2f303d2675b2d9d84f8eab3b016273d694267389
alexzamurca <alexzamurca@yahoo.co.uk>
2020-11-28 13:14:02 +0000

    Q horizontal functionality added: it projects to and lights the scene

--- Assets/Scripts/Q.cs
+++ Assets/Scripts/Q.cs
@@ -6,7 +6,13 @@ public class Q : MonoBehaviour
 {
     public Transform horizontalFirePoint;
     public Transform verticalFirePoint;
-    
+
+    public Animator animator;
+    public GameObject bulletPrefab;
+
+    public float animationTime;
+    private float animationTimeLeft = 0f;
+    private bool shooting = false;
 
     // Update is called once per frame
     void Update()
@@ -15,10 +21,35 @@ public class Q : MonoBehaviour
         {
             Shoot();
         }
+        
+        // Reduce animation time left
+        if (animationTimeLeft > 0)
+        {
+            animationTimeLeft -= 0.01f;
+        }
+        else
+        {
+            if (shooting)
+            {
+              animator.SetBool("HorizontalShooting", false);
+              shooting = false;
+            }
+        }
+        
     }
 
     void Shoot()
     {
-        
+        // Instantiate bullet
+        StartCoroutine(ShootingCoroutine());
+        animator.SetBool("HorizontalShooting", true);
+        animationTimeLeft = animationTime;
+        shooting = true;
+    }
+
+    IEnumerator ShootingCoroutine()
+    {
+        yield return new WaitForSeconds(animationTime/2);
+        Instantiate(bulletPrefab, horizontalFirePoint.position, horizontalFirePoint.rotation);
     }
 }
---
+++ Assets/Scripts/ScoreSystem.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreSystem : MonoBehaviour
+{
+    private Rigidbody2D _rb;
+
+    private void OnCollisionEnter2D(Collision2D other) 
+    {
+        // Decrement speed
+        DecrementVelocity(2f);
+    }
+
+    private void DecrementVelocity(float factor)
+    {
+        _rb = GetComponent<Rigidbody2D>();
+        Vector2 velocity = _rb.velocity;
+        float newX = velocity.x + factor;
+        float newY = velocity.y + factor;
+        _rb.velocity = new Vector2(newX, newY);
+    }
+}
