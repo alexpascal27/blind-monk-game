a16b419dc9b354f5ce1d7babf54c14ba70c075cc
alexzamurca <alexzamurca@yahoo.co.uk>
2020-11-28 11:22:32 +0000

    Beginning to add Q shooting functionality(extension)

---
+++ Assets/Scripts/Q.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Q : MonoBehaviour
+{
+    public Transform horizontalFirePoint;
+    public Transform verticalFirePoint;
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Q"))
+        {
+            Shoot();
+        }
+    }
+
+    void Shoot()
+    {
+        
+    }
+}
