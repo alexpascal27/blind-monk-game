2cee26d79d8dee79c7539feab0bdecd984d99254
alexzamurca <alexzamurca@yahoo.co.uk>
2020-11-29 15:05:50 +0000

    Vertical Shooting works. Should change the way the horizontal shooting works, very hard to use! Next step is adding cooldowns.

--- Assets/Scripts/Q.cs
+++ Assets/Scripts/Q.cs
@@ -9,18 +9,33 @@ public class Q : MonoBehaviour
 
     public Animator animator;
     public GameObject bulletPrefab;
+    public Camera camera;
+    public GameObject horizontalLine;
+    public GameObject verticalLine;
 
     public float animationTime;
     private float animationTimeLeft = 0f;
     private bool shooting = false;
 
+    private bool horizontal = true;
+
+    void Start()
+    {
+        horizontalLine.SetActive(false);
+        verticalLine.SetActive(false);
+    }
+    
     // Update is called once per frame
     void Update()
     {
         if (Input.GetButtonDown("Q"))
         {
-            Shoot();
+            if (horizontal) HorizontalShoot();
+            else VerticalShoot();
+
         }
+
+        RotateLineAccordingToMouse();
         
         // Reduce animation time left
         if (animationTimeLeft > 0)
@@ -31,25 +46,77 @@ public class Q : MonoBehaviour
         {
             if (shooting)
             {
-              animator.SetBool("HorizontalShooting", false);
-              shooting = false;
+                animator.SetBool("HorizontalShooting", false);
+                animator.SetBool("VerticalShooting", false);
+                
+                shooting = false;
             }
         }
         
     }
 
-    void Shoot()
+    void RotateLineAccordingToMouse()
+    {
+        Vector2 mousePosition = camera.ScreenToWorldPoint(Input.mousePosition);
+
+        Debug.Log("Mouse Position:(" + mousePosition.x + " , " + mousePosition.y + ")" + " | Player Position:(" + gameObject.transform.position.x + " , " + gameObject.transform.position.y + ")");
+        
+        // Compare the player and mouse position
+        // If mouse position is above player position
+        if (mousePosition.y > (gameObject.transform.position.y + 1))
+        {
+            if (horizontal)
+            {
+                horizontal = false;
+                horizontalLine.SetActive(false);
+                verticalLine.SetActive(true);
+            }
+        }
+        else
+        {
+            if (!horizontal)
+            {
+              horizontal = true;  
+              horizontalLine.SetActive(true);
+              verticalLine.SetActive(false);
+            }
+        }
+    }
+
+    void HorizontalShoot()
     {
         // Instantiate bullet
-        StartCoroutine(ShootingCoroutine());
+        StartCoroutine(HorizontalShootingCoroutine());
+
         animator.SetBool("HorizontalShooting", true);
         animationTimeLeft = animationTime;
         shooting = true;
     }
 
-    IEnumerator ShootingCoroutine()
+    IEnumerator HorizontalShootingCoroutine()
     {
         yield return new WaitForSeconds(animationTime/2);
         Instantiate(bulletPrefab, horizontalFirePoint.position, horizontalFirePoint.rotation);
+    }
+
+    void VerticalShoot()
+    {
+        // Instantiate bullet
+        StartCoroutine(VerticalShootingCoroutine());
+
+        animator.SetBool("VerticalShooting", true);
+        animationTimeLeft = animationTime;
+        shooting = true;
+    }
+    
+    IEnumerator VerticalShootingCoroutine()
+    {
+        yield return new WaitForSeconds(animationTime/2);
+        
+        // Change to shoot vertically
+        Transform rotatedVerticalFirePoint = verticalFirePoint;
+        rotatedVerticalFirePoint.Rotate(0f, 0f, 90f, Space.Self);
+        Instantiate(bulletPrefab, verticalFirePoint.position, rotatedVerticalFirePoint.rotation);
+        rotatedVerticalFirePoint.Rotate(0f, 0f, -90f, Space.Self);
     }
 }
